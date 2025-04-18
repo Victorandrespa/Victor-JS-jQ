4aadeeedff217a299df79c83077f5d2be3bd4d4a
Victorandrespa <peralta.victor260@gmail.com>
2025-04-18 18:13:30 -0400

    5

--- ops.js
+++ ops.js
@@ -7,3 +7,14 @@ $("#btn-mostrar").on("click", function () {
 $("#btn-ocultarSlow").on("click", function () {
     $("p").hide("slow"); 
 });
+
+
+$("#btn-mostrarFast").on("click", function () {
+    $("#container-parrafos p").show("fast"); 
+});
+$("#btn-slideUp").on("click", function () {
+    $("#container-parrafos p").slideUp(2000); 
+});
+$("#btn-slideDown").on("click", function () {
+    $("#container-parrafos p").slideDown(2000); 
+});
